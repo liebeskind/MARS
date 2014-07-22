0b6e7b75fd71731e619430d8a044917059ea882e
liebeskind <daniel.liebeskind@gmail.com>
2014-07-22 13:54:26 +0300

    Adds location data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -9,11 +9,17 @@ angular.module('starter.controllers', [])
 
 .controller('IncidentDetailCtrl', function($scope, $stateParams, Incidents) {
   $scope.incidentType = Incidents.get($stateParams.incidentId);
-  $scope.thisIncident = {};
-  $scope.thisIncident.date = new Date();
+  $scope.thisIncident = {parentCategory: {name: $scope.incidentType.name}};
+  $scope.thisIncident.currentDate = new Date(); // sets default date/time to now
+  $scope.thisIncident.date = new Date(); // sets default date/time to now
+  $scope.thisIncident.location = {};
+  $scope.thisIncident.location.text = "Use Current Location"; // sets default date/time to now
+  $scope.thisIncident.location.lat = 32.070123; // sets default date/time to now
+  $scope.thisIncident.location.lon = 34.793811; // sets default date/time to now
+
 
   $scope.submitReport = function(thisIncident) {
-  	console.log(thisIncident);
-  	// Incidents.submitIncident(thisIncident);
+  	//should include {Incident ID, Date/Time Stamp, Comment, Parent Category {id, name}, Sub Category {id, name}, Incident Date }
+  	Incidents.submitIncident(thisIncident);
   }
 })
--- www/js/services.js
+++ www/js/services.js
@@ -3,7 +3,7 @@ angular.module('starter.services', [])
 /**
  * A simple example service that returns some data.
  */
-.factory('Incidents', function() {
+.factory('Incidents', function($location) {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
@@ -26,6 +26,11 @@ angular.module('starter.services', [])
     get: function(incidentId) {
       // Simple index lookup
       return incidents[incidentId];
+    },
+    submitIncident: function(incident) {
+      console.log('successfully submitted incident')
+      console.log(incident);
+      $location.path('/tabs/dash')
     }
   }
 });
