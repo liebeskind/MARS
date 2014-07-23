bfe580685e4cd6d614ae6c50cc609c443c36fb64
liebeskind <daniel.liebeskind@gmail.com>
2014-07-23 12:28:18 +0300

    Shortens comment box

--- platforms/ios/www/js/incidentList.js
+++ platforms/ios/www/js/incidentList.js
@@ -4,5 +4,9 @@ angular.module('incidentList.controllers', [])
   $scope.incidents = IncidentList.all();
 
   var fb = new Firebase('https://vivid-fire-3100.firebaseio.com/')
+  
+  // fb.on('value', function(data) {
+  //   $scope.incidents = data.val();
+  // });
 
 })
